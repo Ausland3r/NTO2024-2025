c78ea2ece0ef9cff784ce54f4eded0e313504c07
DenisovDmitrii <denisdmit03@gmail.com>
2024-11-09 18:33:00 +0300

    first try solve taskE

--- Tasks/taskB.py
+++ Tasks/taskB.py
@@ -2,8 +2,10 @@ n, k = list(map(int, input().split()))
 data = sorted(list(map(int, input().split())))
 
 count = data[n - k]
-for i in range(k - 1):
-    count = count & data[n-k + i]
 print(count)
+# for i in range(k - 1):
+#     count = count & data[n-k + i]
+#     print(count)
+# print(count)
 
 
