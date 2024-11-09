ee8ef056c08d3c0c8cdbeca67b1c7f25263a929d
DenisovDmitrii <denisdmit03@gmail.com>
2024-11-09 19:56:33 +0300

    solved taskE?

--- Tasks/taskE.py
+++ Tasks/taskE.py
@@ -1,2 +1,11 @@
-print("Hello World")
+import numpy as np
+n, m = [int(x) for x in input().split()]
+summer = np.zeros((n, m), dtype=int)
+q = int(input())
+for i in range(q):
+    x, y, h, v = [int(z) for z in input().split()]
+    q = np.tri(h, dtype=int)*v
+    summer[n-(y+h)+1:n-(y+h)+1+h, x-1:x+h-1] += q
+
+np.savetxt('summer.txt', summer, delimiter=' ', fmt='%d')
 
