f4b130648eed240797e39de43a2154cd22d12cac
Ausland3r <Ausland3r@yandex.ru>
2024-11-09 19:26:07 +0300

    900 for taskC

--- Tasks/TaskC.sql
+++ Tasks/TaskC.sql
@@ -11,6 +11,5 @@ SELECT capacitor_id FROM condensers c
 JOIN factories f ON c.factory_id = f.factory_id
 JOIN countries co ON f.country_id = co.country_id
 WHERE (co.name = 'Russia' and c.capacity >= 500 and c.voltage <= 200 and c.price <= 40
-and c.capacitor_id not in factory_year) or (co.name != 'Russia' and c.capacity >= 500 and c.voltage <= 200 and c.price <= 20 and c.factory_id not in factory);
-
-
+and c.capacitor_id not in factory_year) or (co.name <> 'Russia' and c.capacity >= 500 and c.voltage <= 200 and c.price <= 20 and c.capacitor_id not in factory)
+ORDER BY c.capacitor_id;
