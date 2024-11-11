e447b6a9a6a9689c771f0a10c1ce75955ad25f61
DenisovDmitrii <denisdmit03@gmail.com>
2024-11-11 10:06:35 +0300

    taskE 1000 points complete

--- Tasks/taskE.py
+++ Tasks/taskE.py
@@ -1,10 +1,12 @@
 import numpy as np
-n, m = [int(x) for x in input().split()]
+f = open('input.txt', 'r')
+n, m = [int(x) for x in f.readline().split()]
 summer = np.zeros((n, m), dtype=int)
-q = int(input())
+q = int(f.readline())
+q1 = np.tri(min(n, m), dtype=int)
 for i in range(q):
-    x, y, h, v = [int(z) for z in input().split()]
+    x, y, h, v = [int(z) for z in f.readline().split()]
     q = np.tri(h, dtype=int)*v
-    summer[n-(y+h)+1:n-(y+h)+1+h, x-1:x+h-1] += q
+    summer[n-(y+h)+1:n-(y+h)+1+h, x-1:x+h-1] += q1[:h, :h] * v
 
 np.savetxt('output.txt', summer, delimiter=' ', fmt='%d')
