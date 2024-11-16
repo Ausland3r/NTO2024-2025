d62b01fd37aef502418a8648ef4501977d019722
Ausland3r <Ausland3r@yandex.ru>
2024-11-16 22:16:29 +0300

    B 1000

--- Tasks/taskB.py
+++ Tasks/taskB.py
@@ -8,5 +8,7 @@ for bit in range(29, -1, -1):
         if (num & (1 << bit)) and (num & result) == result:
             numbers_with_bit.append(num)
 
-
-
+    if len(numbers_with_bit) >= k:
+        result = result | (1 << bit)
+        a = numbers_with_bit
+print(result)
