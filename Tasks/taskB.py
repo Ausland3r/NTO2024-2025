a67904417b9bff46b51f629f63c5db11cb4e52b3
Ausland3r <Ausland3r@yandex.ru>
2024-11-16 21:55:33 +0300

    some changes

--- Tasks/taskB.py
+++ Tasks/taskB.py
@@ -1,11 +1,12 @@
 n, k = list(map(int, input().split()))
-data = sorted(list(map(int, input().split())))
+a = list(map(int, input().split()))
 
-count = data[n - k]
-print(count)
-# for i in range(k - 1):
-#     count = count & data[n-k + i]
-#     print(count)
-# print(count)
+result = 0
+for bit in range(29, -1, -1):
+    numbers_with_bit = []
+    for num in a:
+        if (num & (1 << bit)) and (num & result) == result:
+            numbers_with_bit.append(num)
+
 
 
