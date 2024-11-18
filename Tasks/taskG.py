1a5505bc3c9536fbc56a28528e5431cdbc634cbc
DenisovDmitrii <denisdmit03@gmail.com>
2024-11-18 23:19:53 +0300

    taskG 571 points need fix.

---
+++ Tasks/taskG.py
@@ -0,0 +1,33 @@
+import math
+
+components = [float(x) for x in input().split()]
+frequency = int(input())
+
+R1, R2, C1, C2, R3, R4, C3, C4, R5, R6, C5, C6, R7, R8, C7 = components
+
+C1 *= 1e-9
+C2 *= 1e-9
+C3 *= 1e-9
+C4 *= 1e-9
+C5 *= 1e-9
+C6 *= 1e-9
+C7 *= 1e-9
+
+def calculateFSecondOrder(R1, R2, C1, C2):
+    return 1 / (2 * math.pi * math.sqrt(R1 * R2 * C1 * C2))
+
+def calculateFFirstOrder(R, C):
+    return 1 / (2 * math.pi * R * C)
+
+f1 = calculateFSecondOrder(R1, R2, C1, C2)
+f2 = calculateFSecondOrder(R3, R4, C3, C4)
+f3 = calculateFSecondOrder(R5, R6, C5, C6)
+f4 = calculateFFirstOrder(R7, C7)
+
+f = (f1 + f2 + f3 + f4) / 4
+
+print(round(f))
+
+mag = 1 / math.sqrt(1 + (frequency / f) ** (2*7))
+
+print(f"{mag:.2g}")
